5744fccc6c09db40995d054ad9ff420dfa0cea15
Dmytro Yurin <dmitry.yurin2020@gmail.com>
2026-05-22 19:24:57 +0300

    fix(web): use manual i18n setup instead of plugin

--- apps/web/plugins/i18n-messages.ts
+++
@@ -1,8 +0,0 @@
-import en from '~/locales/en.json'
-import uk from '~/locales/uk.json'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  const i18n = nuxtApp.$i18n as { setLocaleMessage: (locale: string, messages: object) => void }
-  i18n.setLocaleMessage('uk', uk)
-  i18n.setLocaleMessage('en', en)
-})
